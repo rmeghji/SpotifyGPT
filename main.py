208a30ae17d5c74c7c4c36af8400a47e724e7987
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-18 22:52:43 -0400

    spotifygpt

---
+++ main.py
@@ -0,0 +1,86 @@
+from api_keys import OPENAI_API_KEY
+import langchain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain.agents import tool, AgentExecutor
+from langchain.agents.format_scratchpad.openai_tools import format_to_openai_tool_messages
+from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from spotify_tools import check_login, current_track, skip, pause, play, search, play_song, narrow_search, play_album, play_artist, play_playlist
+
+gpt = ChatOpenAI(api_key=OPENAI_API_KEY)
+tool_parser = OpenAIToolsAgentOutputParser()
+
+@tool
+def test_tool():
+    '''Responds to the user after being asked what the color purple tastes like.'''
+    return "tastes weird\n"
+
+@tool
+def exit_tool():
+    '''Exits the program when told to exit.'''
+    exit()
+
+tools = [test_tool, exit_tool, check_login, current_track, skip, pause, play, search, play_song, narrow_search, play_album, play_artist, play_playlist]
+gpt = gpt.bind_tools(tools=tools)
+chat_history = []
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            'system',
+            'You are a helpful chatbot who controls the user\'s Spotify account. '
+            'Before invoking any Spotify-related action, you first check that the user is logged into Spotify and has the necessary permissions. '
+            'You can tell the user what song is currently playing, skip to the next song, search for and play songs/playlists/albums/artists, pause, and unpause songs. '
+            'You should interpret the user telling you to "play" as unpausing the current song. '
+            'You can also search for songs, albums, artists, or playlists that match a query. '
+            # 'If you know for a fact that a user search query is a song, album, artist, or playlist, you can directly perform the corresponding action.'
+            'You should get the search results. Once you\'ve gotten those search results, you should automatically narrow them down by passing in the URI of each category as a list, and then tell the user what the narrowed search results are. '
+            # 'You can then determine whether to perform the corresponding action or ask the user to specify which result they want to play. '
+            'You should then automatically play the best match from the narrowed search results.'
+            'If the user\'s search query closely matches a song in their library, you should play that song. '
+            'If the user\'s search query closely matches an album name in their library, you should play that album. '
+            'If the user\'s search query closely matches the name of an artist in their library, you should play that artist\'s top tracks. '
+            'If the user\'s search query closely matches the name of a playlist in their library, you should play that playlist. '
+            'When automatically deciding to play a song, album, artist, or playlist, you should first prioritize the closest match.' # If there are multiple matches that are equally close, first prioritize songs, then albums, then artists, then playlists. '
+            'If a user gives you a command that corresponds to a Spotify action without specifying that they want to perform that action on Spotify, you should assume that they want to perform that action on Spotify. '
+            'You should not tell a user that you have performed an action unless you have actually performed that action by invoking a tool. If you are unable to perform an action, you should tell the user that you are unable to perform that action. '
+            'You can also respond to general questions unrelated to Spotify, and you can exit the program when told to do so. ',
+        ),
+        MessagesPlaceholder(variable_name='chat_history'),
+        ('user', '{input}'),
+        MessagesPlaceholder(variable_name='agent_scratchpad'),
+    ]
+)
+
+agent = (
+    {
+        'input': lambda x: x['input'],
+        'agent_scratchpad': lambda x: format_to_openai_tool_messages(x['intermediate_steps']),
+        'chat_history': lambda x: x['chat_history']
+    }
+    | prompt
+    | gpt
+    | tool_parser
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+def main():
+    while True:
+        result = agent_executor.invoke({'input': input(), 'chat_history': chat_history})
+        chat_history.extend(
+            [
+                HumanMessage(
+                    content=result['input'],
+                ),
+                AIMessage(
+                    content=result['output'],
+                    agent=agent_executor.agent,
+                ),
+            ]
+        )
+
+if __name__ == "__main__":
+    main()
---
+++ spotify_tools.py
@@ -0,0 +1,145 @@
+from api_keys import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+from time import sleep
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from langchain.agents import tool
+
+scope=['user-read-playback-state', 'user-modify-playback-state', 'user-library-read', 'user-follow-read', 'playlist-read-private', 'user-read-recently-played']
+OAUTH_ARGS = {'client_id': SPOTIPY_CLIENT_ID, 'client_secret': SPOTIPY_CLIENT_SECRET, 'redirect_uri': SPOTIPY_REDIRECT_URI, 'scope': scope}
+# spotify = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI))
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(**OAUTH_ARGS))
+
+# set_auth
+
+@tool
+def check_login():
+    '''Checks that the user is logged into Spotify and has the necessary permissions.'''
+    while True:
+        try:
+            spotify.current_user()
+            break
+        except:
+            # spotify = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
+            spotify = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(**OAUTH_ARGS))
+
+    return "User is logged into Spotify and has the necessary permissions.\n"
+
+@tool
+def current_track():
+    '''Returns the current track playing on the user's Spotify account in the format "Artist Name - Song Title".'''
+    return f"{spotify.current_playback()['item']['artists'][0]['name']} - {spotify.current_playback()['item']['name']}\n"
+
+@tool
+def skip():
+    '''Skips to the next track in the user's Spotify queue. Tells user what song is now playing.'''
+    spotify.next_track()
+    sleep(1)
+    return f"Skipped to the next track. Now playing is {spotify.current_playback()['item']['artists'][0]['name']} - {spotify.current_playback()['item']['name']}.\n"
+
+@tool
+def pause():
+    '''Pauses the current track in the user's Spotify queue.'''
+    if spotify.current_playback()['is_playing']:
+        spotify.pause_playback()
+        return f"Paused the current track.\n"
+    else:
+        return f"Playback is already paused.\n"
+
+@tool
+def play():
+    '''Plays the current track in the user's Spotify queue.'''
+    if not spotify.current_playback()['is_playing']:
+        spotify.start_playback()
+        return f"Playing the current track.\n"
+    else:
+        return f"The current track is already playing.\n"
+    
+@tool
+def search(input: str):
+    '''Takes in a user's request and finds songs, albums, artists, or playlists that match the query.'''
+    results = spotify.search(q=input, limit=2, type='track,album,artist,playlist')
+    songs = '\n'.join([f"{result['name']} by {result['artists'][0]['name']}\nURI: {result['uri']}" for result in results['tracks']['items']])
+    albums = '\n'.join([f"{result['name']} by {result['artists'][0]['name']}\nURI: {result['uri']}" for result in results['albums']['items']])
+    artists = '\n'.join([f"{result['name']}\nURI: {result['uri']}" for result in results['artists']['items']])
+    playlists = '\n'.join([f"{result['name']} by {result['owner']['display_name']}\nURI: {result['uri']}" for result in results['playlists']['items']])
+
+    return f"Search results for '{input}':\nSongs:\n{songs}\n\nAlbums:\n{albums}\n\nArtists:\n{artists}\n\nPlaylists:\n{playlists}\n"
+
+@tool
+def narrow_search(song_uris: list, album_uris: list, artist_uris: list, playlist_uris: list):
+    '''Takes in all URI values from the results of a search query and narrows down the search to items that are saved in the user's Spotify account or artists that the user follows.'''
+    track_uris = song_uris
+    saved_tracks = []
+    if song_uris:
+        saved_tracks_uris = spotify.current_user_saved_tracks_contains(tracks=track_uris)
+        saved_tracks = [uri for i, uri in enumerate(track_uris) if saved_tracks_uris[i]]
+        saved_tracks = [f"{spotify.track(track_id=track_uri)['artists'][0]['name']} - {spotify.track(track_id=track_uri)['name']}" for track_uri in saved_tracks]
+
+    saved_albums = []
+    if album_uris:
+        saved_albums_uris = spotify.current_user_saved_albums_contains(albums=album_uris)
+        saved_albums = [uri for i, uri in enumerate(album_uris) if saved_albums_uris[i]]
+        saved_albums = [f"{spotify.album(album_id=album_uri)['artists'][0]['name']} - {spotify.album(album_id=album_uri)['name']}" for album_uri in saved_albums]
+    
+    followed_artists = []
+    if artist_uris:
+        followed_artists_uris = spotify.current_user_following_artists(ids=artist_uris)
+        followed_artists = [uri for i, uri in enumerate(artist_uris) if followed_artists_uris[i]]
+        followed_artists = [spotify.artist(artist_id=artist_uri)['name'] for artist_uri in followed_artists]
+    
+    followed_playlists = []
+    if playlist_uris:
+        followed_playlists_uris = [spotify.playlist_is_following(playlist_id=playlist_uri.split(':')[-1], user_ids=[spotify.current_user()['id']]) for playlist_uri in playlist_uris]
+        followed_playlists = [uri for i, uri in enumerate(playlist_uris) if followed_playlists_uris[i]]
+        followed_playlists = [spotify.playlist(playlist_id=playlist_uri, fields='name')['name'] for playlist_uri in followed_playlists]
+
+    return f"Saved tracks:\n{saved_tracks}\n\nSaved albums:\n{saved_albums}\n\nFollowed artists:\n{followed_artists}\n\nFollowed playlists:\n{followed_playlists}\n"
+
+# @tool
+# def narrow_search(track_uris: list):
+#     '''Takes in all song URI values from the results of a search query and narrows down the search to tracks that are saved in the user's Spotify account.'''
+#     saved_tracks_uris = spotify.current_user_saved_tracks_contains(tracks=track_uris)
+#     return saved_tracks_uris
+#     # saved_albums_uris = spotify.current_user_saved_albums_contains(albums=album_uris)
+#     # followed_artists_uris = spotify.current_user_following_artists(ids=artist_uris)
+#     # followed_playlists_uris = [spotify.playlist_is_following(playlist_id=playlist_uri.split(':')[-1], user_ids=[spotify.current_user()['id']]) for playlist_uri in playlist_uris]
+
+#     saved_tracks = [f"{spotify.track(track_id=track_uri)['artists'][0]['name']} - {spotify.track(track_id=track_uri)['name']}" for track_uri in saved_tracks_uris]
+#     # saved_albums = [f"{spotify.album(album_id=album_uri)['artists'][0]['name']} - {spotify.album(album_id=album_uri)['name']}" for album_uri in saved_albums_uris]
+#     # followed_artists = [spotify.artist(artist_id=artist_uri)['name'] for artist_uri in followed_artists_uris]
+#     # followed_playlists = [spotify.playlist(playlist_id=playlist_uri)['name'] for playlist_uri in followed_playlists_uris]
+
+#     return f"Saved tracks:\n{saved_tracks}"
+#     # \n\nSaved albums:\n{saved_albums}\n\nFollowed artists:\n{followed_artists}\n\nFollowed playlists:\n{followed_playlists}\n"
+
+@tool
+def play_song(song_name: str):
+    '''Finds and plays a song with the given name on the user's Spotify account.'''
+    spotify.start_playback(uris=[spotify.search(q=song_name, limit=1, type='track')['tracks']['items'][0]['uri']])
+    return f"Playing {song_name}.\n"
+
+@tool
+def play_album(album_name: str):
+    '''Finds and plays an album with the given name on the user's Spotify account.'''
+    spotify.start_playback(context_uri=spotify.search(q=album_name, limit=1, type='album')['albums']['items'][0]['uri'])
+    return f"Playing {album_name}.\n"
+
+@tool
+def play_artist(artist_name: str):
+    '''Finds and plays an artist with the given name on the user's Spotify account.'''
+    artist_id = spotify.search(q=artist_name, limit=1, type='artist')['artists']['items'][0]['id']
+    spotify.start_playback(context_uri=f"spotify:artist:{artist_id}")
+    return f"Playing {artist_name}.\n"
+
+@tool
+def play_playlist(playlist_name: str):
+    '''Finds and plays a playlist with the given name on the user's Spotify account.'''
+    playlist_id = spotify.search(q=playlist_name, limit=1, type='playlist')['playlists']['items'][0]['id']
+    spotify.start_playback(context_uri=f"spotify:playlist:{playlist_id}")
+    return f"Playing {playlist_name}.\n"
+
+@tool
+def queue_song(song_name: str):
+    '''Queues a song with the given name to the user's Spotify queue.'''
+    spotify.add_to_queue(song_name)
+    return f"Queued {song_name}.\n"
