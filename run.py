a17182ee91dd8dad882b100932bb5173a5f4e9ca
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-21 00:31:32 -0400

    get secret key from environ

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_cors import CORS
 from spotify_auth import api_bp
 from main import app_bp
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY', 'dev')
 CORS(app)
 app.register_blueprint(api_bp)
 app.register_blueprint(app_bp)
