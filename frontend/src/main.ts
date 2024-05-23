09eaa069e254e6cf5f4d0f58f1e98e64f6c1f04d
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 17:26:10 -0400

    updated routes for /repo/ base

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,18 +8,18 @@ import axios from 'axios'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: `/`, component: App },
+        { path: `/${import.meta.env.BASE_URL}/`, component: App },
         { path: `/${import.meta.env.BASE_URL}/callback`, component: App, async beforeEnter(to: any, from: any, next: any) {
             try {
                 // const response = await axios.post('/api/callback',
                 //     { code: to.query.code })
                 const response = await axios.post('https://spotifygpt-1267e7132268.herokuapp.com/callback',
                     { code: to.query.code })
-                next(`/`)
+                next(`/${import.meta.env.BASE_URL}/`)
             }
             catch(err) {
                 console.error(err)
-                next(`/`)
+                next(`/${import.meta.env.BASE_URL}/`)
             }
         }}
     ],
