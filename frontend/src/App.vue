4862239774914bc30c17c541cc3b018385d7be0e
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 00:57:01 -0400

    replaced proxy api calls with direct ones

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -65,7 +65,8 @@ export default {
   methods: {
     sendMessage() {
       this.tempInput = this.userInput
-      axios.post('/SpotifyGPT/api/chat', { input: this.userInput }, { headers: { 'Content-Type': 'application/json' } })
+      // axios.post('/SpotifyGPT/api/chat', { input: this.userInput }, { headers: { 'Content-Type': 'application/json' } })
+      axios.post('https://spotifygpt-1267e7132268.herokuapp.com/chat', { input: this.userInput }, { headers: { 'Content-Type': 'application/json' } })
       .then(response => {
         this.chatResponse = response.data.response
         this.tempInput = ''
@@ -80,14 +81,15 @@ export default {
       // window.location.href = '/api/login'
       // axiosRedirector.get('https://spotifygpt-1267e7132268.herokuapp.com/login', { withCredentials: true })
       // axiosRedirector.get('', { withCredentials: true })
-      axios.get('/SpotifyGPT/api/login', { withCredentials: true })
+      // axios.get('/SpotifyGPT/api/login', { withCredentials: true })
+      axios.get('https://spotifygpt-1267e7132268.herokuapp.com/login', { withCredentials: true })
       .then(response => {
         // console.log(response)
         window.location.href = response.data.url
         // window.open(response.data.url, '_blank', 'width=800,height=600')
       })
       .catch(error => {
-        console.error("weeooo error")
+        console.error(error)
       })
     }
   }
