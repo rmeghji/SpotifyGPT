0fa4217a4ec84113605f9d49465f604439f7153a
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 18:00:34 -0400

    removed client-side /callback route altogether, returned to html5 history-based routing, and changed redirect_uris + logic of root path in order to handle the code in the root url

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import axios from 'axios'
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: `/${import.meta.env.BASE_URL}/`, component: App },
+        { path: `/${import.meta.env.BASE_URL}`, component: App, async beforeEnter(to: any, from: any, next: any){
+            try {
+                const response = await axios.post('https://spotifygpt-1267e7132268.herokuapp.com/callback',
+                    { code: to.query.code })
+                next(`/${import.meta.env.BASE_URL}`)
+            }
+            catch(err) {
+                console.error(err)
+                next(`/${import.meta.env.BASE_URL}`)
+            }
+        }},
         { path: `/${import.meta.env.BASE_URL}/callback`, component: App, async beforeEnter(to: any, from: any, next: any) {
             try {
                 // const response = await axios.post('/api/callback',
