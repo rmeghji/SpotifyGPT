59a087234f32cfe443571a74183c1c5eb5fb184b
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 17:19:20 -0400

    removed localhost from a-c-a-origins

--- spotify_auth.py
+++ spotify_auth.py
@@ -106,7 +106,7 @@ class SpotifyManager:
     def login():
         '''New login method that returns jsonified url instead of redirecting.'''
         response = jsonify({'url': SpotifyManager.get_instance().auth_manager.get_authorize_url()})
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173/, https://rmeghji.github.io'
+        response.headers['Access-Control-Allow-Origin'] = 'https://rmeghji.github.io'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'content-type, authorization, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers'
         return response
