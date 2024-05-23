8436e75eeb00a103845a44c706b013ab93ab27d9
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 16:40:53 -0400

    removed secure=false on proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
       '/SpotifyGPT/api': {
         target: 'https://spotifygpt-1267e7132268.herokuapp.com',
         changeOrigin: true,
-        secure: false,
         // rewrite: (path) => path.replace(/^\/api/, '')
         rewrite: (path) => path.replace(/^\/SpotifyGPT\/api/, '')
 
