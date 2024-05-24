130a807c650ea99c967f7e867f134cb822db738e
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-23 20:03:06 -0400

    trying without beforeEnter on main route

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,28 +8,7 @@ import axios from 'axios'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: `/`, component: App, async beforeEnter(to: any, from: any, next: any){
-            console.log("beforeEnter")
-            try {
-                // const response = await axios.post('https://spotifygpt-1267e7132268.herokuapp.com/callback',
-                //     { code: to.query.code })
-                // next(`/${import.meta.env.BASE_URL}`)
-                const urlParams = new URLSearchParams(window.location.search);
-                console.log(urlParams);
-                const code = urlParams.get('code');
-                console.log(code);
-
-                if (code) {
-                    await axios.post('https://spotifygpt-1267e7132268.herokuapp.com/callback', { code: code });
-                    // next(`/${import.meta.env.BASE_URL}/`);
-                }
-                next(`/`);
-            }
-            catch(err) {
-                console.error(err)
-                next(`/`)
-            }
-        }},
+        { path: `/`, component: App },
         { path: `/callback`, component: App, async beforeEnter(to: any, from: any, next: any) {
             try {
                 // const response = await axios.post('/api/callback',
