97e97198b748f4b871f76603a7729d549589289a
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-27 17:10:02 -0400

    secure=True on session

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ app.config['SESSION_COOKIE_PATH'] = '/'
 app.config['SESSION_TYPE'] = 'filesystem'
 # app.config['SESSION_COOKIE_DOMAIN'] = 'https://spotifygpt.pages.dev/chat'
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 app.register_blueprint(api_bp)
 app.register_blueprint(app_bp)
 CORS(app,
