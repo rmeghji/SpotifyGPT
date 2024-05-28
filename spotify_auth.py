a3c6b27d249b0ae9d574ca4e14ef17924d81b966
Rayhan Meghji <rayhan_meghji@brown.edu>
2024-05-27 22:07:02 -0400

    app.chat

--- spotify_auth.py
+++ spotify_auth.py
@@ -39,7 +39,7 @@ class SpotifyManager:
         # response = redirect('http://localhost:5173/')
         session['spotify_access_token'] = token
         # response = redirect('http://localhost:5173/')
-        response = redirect(url_for('app'))
+        response = redirect(url_for('app.chat'))
         # response = redirect(url_for('app.chat'))
         # response.headers['Access-Control-Allow-Origin'] = '*'
         # response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
